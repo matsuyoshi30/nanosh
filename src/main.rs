833977cc5716a28e5ed94e47b40026c6c0a58bae
matsuyoshi30 <sfbgwm30@gmail.com>
2021-12-19 02:05:33 +0900

    Add echo command

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,24 @@ use shlex;
 use std::io::{stdin, stdout, Write};
 use std::process;
 
+trait Executor {
+    fn execute(&self);
+}
+
+struct EchoCommand {
+    args: Vec<String>,
+}
+
+impl Executor for EchoCommand {
+    fn execute(&self) {
+        for i in &self.args {
+            print!("{}", i);
+            print!(" ");
+        }
+        println!();
+    }
+}
+
 fn main() {
     ctrlc::set_handler(move || {
         println!();
@@ -19,8 +37,21 @@ fn main() {
         let mut input = String::new();
         stdin().read_line(&mut input).expect("no given input");
 
-        if let Some(result) = shlex::split(&input) {
-            println!("{:?}", result);
+        if let Some(mut result) = shlex::split(&input) {
+            let cmd = result.remove(0);
+            match cmd.as_ref() {
+                "echo" => {
+                    let mut arguments = Vec::new();
+                    for i in result {
+                        arguments.push(i);
+                    }
+                    let cmd = EchoCommand { args: arguments };
+                    cmd.execute();
+                }
+                _ => {
+                    println!("unknown command");
+                }
+            }
         } else {
             println!("unknown shell commands");
         }
