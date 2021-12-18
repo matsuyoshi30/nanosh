4cc78f9b7048e1a8d98d8aa9869956e1373b62eb
matsuyoshi30 <sfbgwm30@gmail.com>
2021-12-18 20:09:57 +0900

    Get user's input and print it

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io::{stdin, stdout, Write};
+
 fn main() {
-    println!("Hello, world!");
+    println!("nanosh start");
+    loop {
+        print!(">> ");
+        stdout().flush().unwrap();
+
+        let mut input = String::new();
+        stdin().read_line(&mut input).expect("no given input");
+        println!("{}", input);
+    }
 }
