a3a600513ddb0bc535d9dc235b9d44509e73df58
matsuyoshi30 <sfbgwm30@gmail.com>
2021-12-18 20:24:07 +0900

    Handling Ctrl+C

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
+use ctrlc;
 use std::io::{stdin, stdout, Write};
+use std::process;
 
 fn main() {
+    ctrlc::set_handler(move || {
+        println!();
+        println!("nanosh is finishing...");
+        process::exit(0);
+    })
+    .expect("Error setting Ctrl-C handler");
+
     println!("nanosh start");
     loop {
         print!(">> ");
