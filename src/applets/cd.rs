c512a0539ad7049e03590cd988a697808fdc05ff
matsuyoshi30 <sfbgwm30@gmail.com>
2021-12-19 23:41:34 +0900

    Handle error when trying to cd to invalid directory

--- src/applets/cd.rs
+++ src/applets/cd.rs
@@ -10,11 +10,14 @@ pub struct CdCommand<'a> {
 impl Executor for CdCommand<'_> {
     fn execute(&self, shell: &mut Shell) {
         let path = Path::new(&self.arg);
-        if path.is_relative() {
-            let abspath = &shell.current_dir.as_path().join(path);
-            shell.current_dir = abspath.canonicalize().unwrap();
-        } else {
+        if path.is_absolute() {
             shell.current_dir = path.canonicalize().unwrap();
+        } else {
+            let abspath = &shell.current_dir.as_path().join(path);
+            match abspath.canonicalize() {
+                Ok(td) => shell.current_dir = td,
+                Err(_) => println!("No such file or directory"),
+            }
         }
     }
 }
