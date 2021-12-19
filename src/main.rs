aa1a240a5b5d9c778e955e4cbd09584f0960b7e9
matsuyoshi30 <sfbgwm30@gmail.com>
2021-12-19 14:18:28 +0900

    Add export command

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,6 @@ use std::env;
 use std::io::{stdin, stdout, Write};
 use std::process;
 
-#[warn(dead_code)]
 struct Shell {
     envs: HashMap<String, String>,
 }
@@ -18,11 +17,40 @@ impl Shell {
     fn new(envs: HashMap<String, String>) -> Self {
         Shell { envs: envs }
     }
+
+    fn set_env(&mut self, key: String, val: String) {
+        self.envs.insert(key, val);
+        println!("env length is {}", self.envs.len());
+    }
+
+    fn del_env(&mut self, key: String) {
+        self.envs.remove(&key);
+    }
+
     fn run(&mut self, cmd: String, args: Vec<String>) {
         match cmd.as_ref() {
             "echo" => {
                 let cmd = EchoCommand { args: args };
-                cmd.execute();
+                cmd.execute(self);
+            }
+            "export" => {
+                let mut is_delete = false;
+                let mut is_print = false;
+                let mut arguments = Vec::new();
+                for arg in args {
+                    match arg.as_ref() {
+                        "-n" => is_delete = true,
+                        "-f" => is_print = true,
+                        v => arguments.push(v.to_string()),
+                    }
+                }
+
+                let cmd = ExportCommand {
+                    delete: is_delete,
+                    print: is_print,
+                    args: arguments,
+                };
+                cmd.execute(self);
             }
             _ => {
                 println!("unknown command");
@@ -32,7 +60,7 @@ impl Shell {
 }
 
 trait Executor {
-    fn execute(&self);
+    fn execute(&self, shell: &mut Shell);
 }
 
 struct EchoCommand {
@@ -40,13 +68,42 @@ struct EchoCommand {
 }
 
 impl Executor for EchoCommand {
-    fn execute(&self) {
+    fn execute(&self, _: &mut Shell) {
         for i in &self.args {
             write(libc::STDOUT_FILENO, i.as_bytes()).ok();
             write(libc::STDOUT_FILENO, " ".as_bytes()).ok();
         }
         write(libc::STDOUT_FILENO, "\n".as_bytes()).ok();
-        unsafe { libc::_exit(0) };
+    }
+}
+
+struct ExportCommand {
+    delete: bool,
+    print: bool,
+    args: Vec<String>,
+}
+
+impl Executor for ExportCommand {
+    fn execute(&self, shell: &mut Shell) {
+        if self.print {
+            for (key, val) in &shell.envs {
+                println!("export {}={}", key, val);
+            }
+        } else {
+            for arg in &self.args {
+                if self.delete {
+                    shell.del_env(arg.to_string());
+                } else {
+                    let kv: Vec<&str> = arg.split("=").collect();
+                    if kv.len() != 2 {
+                        write(libc::STDERR_FILENO, "invalid argument\n".as_bytes()).ok();
+                        unsafe { libc::_exit(1) };
+                    } else {
+                        shell.set_env(kv[0].to_string(), kv[1].to_string());
+                    }
+                }
+            }
+        }
     }
 }
 
